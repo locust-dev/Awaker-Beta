6b5b048f811cf9c0dc64aa38f120c60acdd52d28
Ilya Turin <rrrr.ty30@gmail.com>
2022-03-22 14:04:04 +0300

    SPM

--- Awaker/Services/Database/DatabaseService.swift
+++ Awaker/Services/Database/DatabaseService.swift
@@ -5,7 +5,7 @@
 //  Created by Ilya Turin on 11.12.2021.
 //
 
-import FirebaseDatabase
+//import FirebaseDatabase
 
 protocol DatabaseServiceInput: NetworkClient {
     func getData<Model: Decodable>(_ key: DatabasePath, modelType: Model.Type, completion: @escaping (Result<Model, ErrorModel>) -> Void)
@@ -17,13 +17,13 @@ final class DatabaseService {
     
     private enum DatabaseReference {
         
-        static let realtime = Bundle.main.object(forInfoDictionaryKey: "DatabaseReference")
+        // static let realtime = Bundle.main.object(forInfoDictionaryKey: "DatabaseReference")
     }
     
     
     // MARK: - Properties
     
-    private let databaseReference = Database.database(url: DatabaseReference.realtime as! String).reference()
+//    private let databaseReference = Database.database(url: DatabaseReference.realtime as! String).reference()
 
 }
 
@@ -37,15 +37,15 @@ extension DatabaseService: DatabaseServiceInput {
         
         globalQueue {
             
-            self.databaseReference.child(key.stringPath).getData { [weak self] error, data in
-                
-                guard let dataValue = data.value, error == nil else {
-                   // completion(.failure(.serverError))
-                    return
-                }
-                
-                self?.parse(rawData: dataValue, type: modelType, completion: completion)
-            }
+//            self.databaseReference.child(key.stringPath).getData { [weak self] error, data in
+//
+//                guard let dataValue = data.value, error == nil else {
+//                   // completion(.failure(.serverError))
+//                    return
+//                }
+//
+//                self?.parse(rawData: dataValue, type: modelType, completion: completion)
+//            }
         }
         
     }
